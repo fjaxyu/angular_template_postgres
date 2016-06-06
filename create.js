d857a9b686ca5f81782f4922afda5f7d11ef1363
Wesley Braithwaite <wbraithwaite@me.com>
2016-06-06 17:17:01 -0600

    Fixed a bug in the create function.

--- create.js
+++ create.js
@@ -12,7 +12,7 @@ postgres.query(query, function(data){
     
         console.log('Done. Creating Logs.');
         
-        var query3 = "create table logs (log_id serial, public_message text, log_message text, date_found date, date_fixed date, public boolean, public_title text, priority text, device text, type text, minutes integer, estimated_minutes integer, fixed boolean, assigned_to text, subgroup text, group text, fixed_by text, date_due date, created_by text, updated_by text, last_updated date, primary key (log_id));";
+        var query3 = "create table logs (log_id serial, public_message text, log_message text, date_found date, date_fixed date, public boolean, public_title text, priority text, device text, type text, minutes integer, estimated_minutes integer, fixed boolean, assigned_to text, subgroup text, \"group\" text, fixed_by text, date_due date, created_by text, updated_by text, last_updated date, primary key (log_id));";
         postgres.query(query3, function(data){
         
             console.log('Done. Creating Feedback.');
